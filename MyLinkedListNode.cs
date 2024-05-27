7e68885395769e9e1d31cdd7bd1509ad8bd2a39f
George <148636575+Ge0rgeKA@users.noreply.github.com>
2024-05-27 23:22:29 +0300

    Add files via upload

---
+++ MyLinkedListNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson25._05
+{
+    public class MyLinkedListNode
+    {
+        private MyLinkedListNode next;
+        //private MyLinkedListNode previous;
+        internal int count = 0; //Добавил счетчик, но не придумал как реализовать его
+        public int Value { get; set; }
+
+        public int Count
+        {
+            get { return count; }
+        }
+        public MyLinkedListNode Next
+        {
+            get { return next; }
+            set
+            {
+                next = value;
+                count++; //попытки реализовать счетчик
+            }
+        }
+
+        public void PrintAllLinckedNodes()
+        {
+            Console.WriteLine(Value);
+            MyLinkedListNode n = Next;
+            while (n != null)
+            {
+                Console.WriteLine(n.Value);
+                n = n.Next;
+            }
+        }
+
+        public void AddNode(int V)
+        {
+            MyLinkedListNode n = this;
+            
+            do
+            {
+                n = n.Next;
+            } while (n != null) ;
+
+            n = new MyLinkedListNode 
+            { 
+                Value = V, 
+                Next = null 
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Lesson25._05;
+
+namespace Lesson25;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyLinkedListNode m = new MyLinkedListNode
+        {
+            Value = 1,
+            Next = new MyLinkedListNode
+            {
+                Value = 2,
+                Next = new MyLinkedListNode
+                {
+                    Value = 3,
+                    Next = null
+                }
+            }
+        };
+
+        m.PrintAllLinckedNodes();
+        m.AddNode(11);
+        m.PrintAllLinckedNodes();
+    }
+}
